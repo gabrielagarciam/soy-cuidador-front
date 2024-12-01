56ff2f6c761d24273e4fe6ddcc161a3019889fa5
Gabriela García <44621906+gabrielagarciam@users.noreply.github.com>
2024-12-01 20:22:25 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import routes from "./routes";
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior() {
+    return { top: 0 }; // Reinicia el scroll al redirigir
+  },
 });
 
 router.beforeEach(async (to, from, next) => {
