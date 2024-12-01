37874ac4041709ad372dc4525349b6b6dd3e12b2
Gabriela García <44621906+gabrielagarciam@users.noreply.github.com>
2024-12-01 15:24:56 +0100

    adding speed-insights

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div  class="main-app-container">
     <RouterView />
+    <SpeedInsights/>
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import { SpeedInsights } from "@vercel/speed-insights/vue"
 </script>
 
 <style lang="scss" scoped></style>
