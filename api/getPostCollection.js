d9e60f25b4591a7eff78ea680dda5f6e15acf450
Gabriela García <44621906+gabrielagarciam@users.noreply.github.com>
2024-12-22 20:15:10 +0100

    Update getPostCollection.js

--- api/getPostCollection.js
+++ api/getPostCollection.js
@@ -1,26 +1,26 @@
-import { connectToDatabase, COLLECTIONS } from '../db.js';
+import { connectToDatabase, COLLECTIONS } from "../db.js";
 import { getHandler } from "../utils/index.js";
 
 const handler = async function handler(req, res) {
-    const {db} = await connectToDatabase();
+  const { db } = await connectToDatabase();
 
-    const documents = await db
-        .collection(COLLECTIONS.POSTS)
-        .find({})
-        .project({
-            slug: 1,
-            id: 1,
-            date: 1,
-            image: 1,
-            title: 1,
-            description: 1,
-            shapePath: 1,
-            viewCount: 1,
-            likeCount: 1,
-        })
-        .toArray();
+  const documents = await db
+    .collection(COLLECTIONS.POSTS)
+    .find({ public: true })
+    .project({
+      slug: 1,
+      id: 1,
+      date: 1,
+      image: 1,
+      title: 1,
+      description: 1,
+      shapePath: 1,
+      viewCount: 1,
+      likeCount: 1,
+    })
+    .toArray();
 
-    res.status(200).json({documents});
+  res.status(200).json({ documents });
 };
 
-export default getHandler(handler)
+export default getHandler(handler);
