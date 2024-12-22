1ddcace58b5d22e19756c00d2646f410a882de7c
gaaabo <gabrielpinto16@outlook.com>
2024-12-22 23:03:08 +0100

    Simplify isLocalhost function logic
    Removed unnecessary checks for IP addresses in the isLocalhost function.

--- utils/isLocalhost.js
+++ utils/isLocalhost.js
@@ -1,9 +1,3 @@
 export function isLocalhost(req) {
-    const isLocalhostHost = req.headers.host?.includes('localhost');
-
-    const isLocalhostIP =
-        req.socket.remoteAddress === '127.0.0.1' ||
-        req.socket.remoteAddress === '::1';
-
-    return isLocalhostHost || isLocalhostIP;
+    return req.headers.host?.includes('localhost');
 }
