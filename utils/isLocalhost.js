23e83b709a2cff37a6372f3d9213b0b248169d65
gaaabo <gabrielpinto16@outlook.com>
2024-12-22 23:01:11 +0100

    "Remove local network check from isLocalhost utility
    Simplified the isLocalhost function by removing the isLocalNetwork check.

--- utils/isLocalhost.js
+++ utils/isLocalhost.js
@@ -5,8 +5,5 @@ export function isLocalhost(req) {
         req.socket.remoteAddress === '127.0.0.1' ||
         req.socket.remoteAddress === '::1';
 
-    const isLocalNetwork = req.headers.host?.startsWith('192.168.') ||
-        req.headers.host?.startsWith('10.');
-
-    return isLocalhostHost || isLocalhostIP || isLocalNetwork;
+    return isLocalhostHost || isLocalhostIP;
 }
