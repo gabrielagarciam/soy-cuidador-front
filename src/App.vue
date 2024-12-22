7863037f39b6a1a8989e100d673fb5f254486f20
gaaabo <gabrielpinto16@outlook.com>
2024-12-22 21:12:33 +0100

    Add Analytics component to App.vue
    Integrated the Vercel Analytics component into the main app container.

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <div  class="main-app-container w-full h-full">
     <RouterView />
     <SpeedInsights/>
+    <Analytics />
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
 import { SpeedInsights } from "@vercel/speed-insights/vue"
+import { Analytics } from "@vercel/analytics/vue";
 </script>
 
 <style lang="scss" scoped></style>
